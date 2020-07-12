766655caa8dc4d6977980b10679ea322ca4fc225
justTryingToCodeHere <68203251+justTryingToCodeHere@users.noreply.github.com>
2020-07-12 16:50:00 -0400

    Add files via upload

---
+++ caseinsensitivebaby.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+let prefix = "A! ";
+client.on("message", (message) => {
+
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  if (message.content.startsWith(prefix + "Hello") || message.content.startsWith(prefix + "hello") || message.content.startsWith(prefix + "Hey") || message.content.startsWith(prefix + "hey")) {
+    message.channel.send("Hello there.");
+  } else
+  if (message.content.startsWith(prefix + "I love you") || message.content.startsWith(prefix + "i love you")) {
+    message.reply("I love you too.");
+  }
+});
+
+client.login('NzMxMzEyOTc4MjY3NDcxOTQ1.XwkWEA.BQsJRI4v-7gRtY1MDsQH7v8b2Ts');
